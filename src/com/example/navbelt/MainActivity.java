f3cb0c65ee0b4770d229237f6b4cac4d1b088d39
Amy Zhou <amyzhou@princeton.edu>
2013-04-18 01:22:51 -0400

    plays tones on command now!

--- src/com/example/navbelt/MainActivity.java
+++ src/com/example/navbelt/MainActivity.java
@@ -5,22 +5,23 @@ import android.content.Context;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
-import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	private AudioManager am;
+	private AudioTrack audioTrack;
     private final int duration = 3; // seconds
     private final int sampleRate = 8000;
     private final int numSamples = duration * sampleRate;
     private final double sample[] = new double[numSamples];
-    private final double freqOfTone = 440; // hz
 
     private final byte generatedSnd[] = new byte[2 * numSamples];
-    void genTone(){
+    void genTone(int freq){
         // fill out the array
         for (int i = 0; i < numSamples; ++i) {
-            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freq));
         }
 
         // convert to 16 bit pcm sound array
@@ -37,25 +38,37 @@ public class MainActivity extends Activity {
     }
 
     void playSound(){
-        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+    	// kill any currently playing sounds
+    	if (audioTrack.getPlayState() == AudioTrack.PLAYSTATE_PLAYING) {
+        	audioTrack.flush();
+        	audioTrack.stop();
+        	audioTrack.release();
+    	}
+//    	am.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_SAME, AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
                 sampleRate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
                 AudioFormat.ENCODING_PCM_16BIT, numSamples,
                 AudioTrack.MODE_STATIC);
+
         audioTrack.write(generatedSnd, 0, generatedSnd.length);
         audioTrack.play();
     }
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		this.am = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, numSamples,
+                AudioTrack.MODE_STATIC);
 /*		AudioManager am = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
 	
 		ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_MUSIC, ToneGenerator.MAX_VOLUME);
 		tg.startTone(ToneGenerator.TONE_DTMF_1, 99999);*/
-		genTone();
+		genTone(440);
 		playSound();
 	}
 
@@ -66,4 +79,25 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void playToneLeft(View view){
+		playTone(87);
+	}
+	
+	public void playToneForward(View view){
+		playTone(183);
+	}
+
+	public void playToneRight(View view){
+		playTone(323);
+	}
+
+	public void playToneStop(View view){
+		playTone(467);
+	}
+	
+	void playTone(int freq) {
+		
+		genTone(freq);
+		playSound();
+	}
 }
